1e973d662dc20ca7e9b6f06612e1507b277f3e84
valentineejk <valentineejk@gmail.com>
2023-04-19 20:29:54 +0100

    Initial Commit

---
+++ SwiftUIThinking/SwiftUIThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingApp.swift
+//  SwiftUIThinking
+//
+//  Created by Valentineejk on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
