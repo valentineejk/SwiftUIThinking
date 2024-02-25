d9c02069da2b548ab883c3fa20df5788070b18e4
valentineejk <valentineejk@gmail.com>
2024-02-25 13:48:20 +0100

    alert func

---
+++ SwiftUIThinking/AlertSwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  AlertSwiftUIView.swift
+//  SwiftUIThinking
+//
+//  Created by Valentineejk on 24/02/2024.
+//
+
+import SwiftUI
+
+struct AlertSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertSwiftUIView()
+}
