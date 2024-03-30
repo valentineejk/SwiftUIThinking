8d091794d1d7da8eb3007bea921187000b9b7468
valentineejk <valentineejk@gmail.com>
2024-03-30 22:39:20 +0100

    added picker

---
+++ SwiftUIThinking/PickerSwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  PickerSwiftUIView.swift
+//  SwiftUIThinking
+//
+//  Created by Valentineejk on 30/03/2024.
+//
+
+import SwiftUI
+
+struct PickerSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerSwiftUIView()
+}
